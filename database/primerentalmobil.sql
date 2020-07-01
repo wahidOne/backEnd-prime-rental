5e1864ddc95bd49a034d5b2d5b83b99916696d90
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2020-07-01 15:28:17 +0700

    kesalahan ekspor db

--- database/primerentalmobil.sql
+++ database/primerentalmobil.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 01 Jul 2020 pada 09.21
+-- Waktu pembuatan: 01 Jul 2020 pada 10.27
 -- Versi server: 10.4.11-MariaDB
 -- Versi PHP: 7.4.3
 
