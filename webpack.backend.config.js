3a05e7196d857595eb0ee13464ab5815d4b7c2ab
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2020-07-06 08:08:07 +0700

    file webpack config dibagi menjadi 2 file

---
+++ webpack.backend.config.js
@@ -0,0 +1,42 @@
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+	entry: {
+		transactions: "./assets/backEnd/Apps/Transactions/Transactions.js",
+	},
+	watch: true,
+	watchOptions: {
+		ignored: /node_modules/,
+		aggregateTimeout: 200,
+		poll: 1000,
+	},
+	mode: "production",
+	output: {
+		filename: "[name]/[name].js",
+		path: path.resolve(__dirname, "assets/backEnd/js/_costum/"),
+	},
+	plugins: [
+		// new webpack.ProvidePlugin({
+		// 	$: "jquery",
+		// 	jQuery: "jquery",
+		// }),
+		new CleanWebpackPlugin(),
+	],
+
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/env"],
+						plugins: ["transform-class-properties"],
+					},
+				},
+			},
+		],
+	},
+};
